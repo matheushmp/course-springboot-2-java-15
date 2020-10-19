1c248ab738d1e63abbca69c4287a54c95313a7e7
matheushmp <hefestoce@gmail.com>
2020-10-19 11:15:00 -0300

    H2 database

--- src/main/java/com/maingroup/course/entities/User.java
+++ src/main/java/com/maingroup/course/entities/User.java
@@ -2,6 +2,13 @@ package com.maingroup.course.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -12,6 +19,8 @@ public class User implements Serializable {
 				+ "]";
 	}
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
