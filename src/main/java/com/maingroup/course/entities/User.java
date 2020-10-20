6c06dff8a7b135f1691f1af47ead1a367e8475da
matheushmp <hefestoce@gmail.com>
2020-10-20 15:50:09 -0300

    Order table part 1

--- src/main/java/com/maingroup/course/entities/User.java
+++ src/main/java/com/maingroup/course/entities/User.java
@@ -1,14 +1,19 @@
 package com.maingroup.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -27,6 +32,11 @@ public class User implements Serializable {
 	private String phone;
 	private String password;
 	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
+
+
 	public User() {
 		
 	}
@@ -78,6 +88,10 @@ public class User implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
