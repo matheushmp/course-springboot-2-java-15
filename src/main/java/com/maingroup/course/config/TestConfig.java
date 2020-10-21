cc8936ece7e5780b8e6e9cde7bff79d836dcbe6d
matheushmp <hefestoce@gmail.com>
2020-10-21 10:44:17 -0300

    Category

--- src/main/java/com/maingroup/course/config/TestConfig.java
+++ src/main/java/com/maingroup/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.maingroup.course.entities.Category;
 import com.maingroup.course.entities.Order;
 import com.maingroup.course.entities.User;
 import com.maingroup.course.entities.enums.OrderStatus;
+import com.maingroup.course.repositories.CategoryRepository;
 import com.maingroup.course.repositories.OrderRepository;
 import com.maingroup.course.repositories.UserRepository;
 
@@ -24,6 +26,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -35,6 +40,12 @@ public class TestConfig implements CommandLineRunner{
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT, u1); 
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
 
 		
 	}
